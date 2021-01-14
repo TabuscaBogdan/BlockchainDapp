be6816c655054285dd11836bf879a5ea96c8a9e8
Bogdan <bogdan.tabusca@gmail.com>
2021-01-14 18:10:51 +0200

    Add project files.

---
+++ BlockchainDapp/Contracts/Producer.sol
@@ -0,0 +1,33 @@
+﻿pragma solidity ^0.7.0;
+
+contract Ownable {
+    address payable owner;
+    
+    constructor () {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+}
+
+contract Producer is Ownable{
+    
+    string name;
+    uint16 reputation;
+    string expertiseCategory;
+    
+}
+
+contract Manager is Producer {
+    
+}
+
+contract Product is Ownable {
+    string description;
+    uint DEVcost;
+    uint REVreward;
+    Manager manager;
+}
---
+++ BlockchainDapp/wwwroot/js/BlockchainConnect.js
@@ -0,0 +1,36 @@
+﻿
+async function InitializeEthereumConnection() {
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            await ethereum.enable();
+        } catch (err) {
+            $('#status').html('User denied account access', err)
+        }
+    } else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider)
+    } else {
+        $('#status').html('No Metamask (or other Web3 Provider) installed')
+    }
+}
+
+const initPayButton = () => {
+    $('.pay-button').click(() => {
+        // paymentAddress is where funds will be send to
+        const paymentAddress = '0x192c96bfee59158441f26101b2db1af3b07feb40'
+        const amountEth = 1
+
+        web3.eth.sendTransaction({
+            to: paymentAddress,
+            value: web3.toWei(amountEth, 'ether')
+        }, (err, transactionId) => {
+            if (err) {
+                console.log('Payment failed', err)
+                $('#status').html('Payment failed')
+            } else {
+                console.log('Payment successful', transactionId)
+                $('#status').html('Payment successful')
+            }
+        })
+    })
+}
